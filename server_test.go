8d4ed43b29a2499602f2ea099c4d9a23835304b1
Lewis Jones <lewis.jones@smartpension.co.uk>
2020-12-29 17:28:09 +0000

    Add websocket helper functions for tests

--- server_test.go
+++ server_test.go
@@ -121,15 +121,11 @@ func TestGame(t *testing.T) {
 
 		wsURL := "ws" + strings.TrimPrefix(server.URL, "http") + "/ws"
 
-		ws, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
-		if err != nil {
-			t.Fatalf("could not open a ws connection on %s %v", wsURL, err)
-		}
-		defer ws.Close()
+		conn := mustDialWS(t, wsURL)
 
-		if err := ws.WriteMessage(websocket.TextMessage, []byte(winner)); err != nil {
-			t.Fatalf("could not send message over ws connection %v", err)
-		}
+		defer conn.Close()
+
+		writeWSMessage(t, conn, winner)
 
 		time.Sleep(10 * time.Millisecond)
 		AssertPlayerWin(t, store, winner)
@@ -152,4 +148,20 @@ func mustMakePlayerServer(t *testing.T, store PlayerStore) *PlayerServer {
 	}
 
 	return server
+}
+
+func mustDialWS(t *testing.T, url string) *websocket.Conn {
+	ws, _, err := websocket.DefaultDialer.Dial(url, nil)
+
+	if err != nil {
+		t.Fatalf("could not open a ws connection on %s %v", url, err)
+	}
+
+	return ws
+}
+
+func writeWSMessage(t *testing.T, conn *websocket.Conn, message string) {
+	if err := conn.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
+		t.Fatalf("could not send message over ws connection %v", err)
+	}
 }
